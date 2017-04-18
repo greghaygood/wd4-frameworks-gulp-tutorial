affd25a1ab60827e1a31820ccf959ffd98e6c662
Chris Silich <chris.silich@gmail.com>
2017-04-17 20:11:18 -0400

    added sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
   concat = require('gulp-concat'),
   del = require('del'),
+  scss = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
   autoprefixer = require('gulp-autoprefixer');
 
 var browserSync = require('browser-sync').create();
@@ -17,8 +19,11 @@ gulp.task('browserSync', function() {
 });
 
 gulp.task('styles', function() {
-	return gulp.src('src/css/**/*.css')
+	return gulp.src('src/scss/**/*.scss')
+	.pipe(sourcemaps.init())
+	.pipe(scss())
 	.pipe(autoprefixer('last 4 version'))
+	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('assets/css'))
 	.pipe(browserSync.reload({stream: true}));
 });
@@ -39,7 +44,7 @@ gulp.task('clean', function() {
 gulp.task('default', ['clean', 'styles', 'scripts']);
 
 gulp.task('watch', ['clean', 'styles', 'scripts', 'browserSync'], function() {
-  gulp.watch('src/css/**/*.css', ['styles']);
+  gulp.watch('src/scss/**/*.scss', ['styles']);
   gulp.watch('src/js/**/*.js', ['scripts']);
   
 });
